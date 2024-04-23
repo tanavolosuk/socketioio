dc712201660d1f0f96ffcaaea8876d8339ba4dc8
Таня <tanavolosuk92@gmail.com>
2024-04-23 10:18:45 +0500

    исправление небольшого косячка

--- lib/modules/chat/widget/bubble_message.dart
+++ lib/modules/chat/widget/bubble_message.dart
@@ -3,57 +3,39 @@ import 'package:socketioio/core/colors.dart';
 import 'package:socketioio/data/models/chat_message/chat_message.dart';
 
 class BubbleMessage extends StatelessWidget {
+  const BubbleMessage({super.key, required this.message, required this.itsMe});
+
   final ChatMessage message;
   final bool itsMe;
-  const BubbleMessage({super.key, required this.message, required this.itsMe});
 
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: itsMe ? Alignment.topRight : Alignment.topLeft,
+      alignment: itsMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-        padding: const EdgeInsets.all(10),
-        margin: EdgeInsets.only(
-            top: 5, bottom: 10, left: itsMe ? 60 : 15, right: itsMe ? 10 : 60),
-        decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.white10,
-              spreadRadius: 1,
-              blurRadius: 10,
-              offset: Offset(2, 2),
-            )
-          ],
-          borderRadius: BorderRadius.circular(10),
-          color: itsMe
-              ? AppColors.secondPrimeryColor
-              : AppColors.thirdPrimeryColor,
-        ),
-        child: Column(
-          crossAxisAlignment:
-              itsMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (!itsMe)
-              Text(
-                message.username,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                  color: AppColors.firstPrimeryColor,
+          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: itsMe
+                  ? AppColors.thirdPrimeryColor
+                  : AppColors.secondPrimeryColor,
+              borderRadius: BorderRadius.circular(10)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (!itsMe)
+                Text(
+                  message.username,
+                  style: const TextStyle(
+                      color: AppColors.firstPrimeryColor,
+                      fontWeight: FontWeight.bold),
                 ),
+              Text(
+                message.message,
+                style: TextStyle(color: itsMe ? Colors.black : Colors.white),
               ),
-            if (!itsMe)
-              const SizedBox(
-                height: 5,
-              ),
-            Text(
-              message.message,
-              style: const TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
